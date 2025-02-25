036942900dc23e8d83f3d480e707cdf820315112
gildossantos <gilbertodossantos74@gmail.com>
2025-02-25 12:48:04 -0500

    DB-1000: skeleton for classical allele script

---
+++ src/report_classical_alleles.py
@@ -0,0 +1,189 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Report classical alleles info.
+
+Author(s):
+    Gil dos Santos dossantos@morgan.harvard.edu
+
+Usage:
+    report_classical_alleles.py [-h] [-v VERBOSE] [-c CONFIG]
+
+Example:
+    python report_classical_alleles.py -v -c /path/to/config.cfg
+
+"""
+
+import argparse
+from harvdev_utils.general_functions import (
+    generic_FB_tsv_dict, tsv_report_dump
+)
+from harvdev_utils.psycopg_functions import (
+    set_up_db_reading, connect
+)
+
+# Global variables for the output file. Header order will match list order below.
+report_label = 'dmel_classical_and_insertion_allele_descriptions'
+report_title = 'FlyBase D. melanogaster classical and insertion allele descriptions report'
+header_list = [
+    'Allele (symbol)',
+    'Allele (id)',
+    'Gene (symbol)',
+    'Gene (id)',
+    'Allele Class (term)',
+    'Allele Class (id)',
+    'Insertion (symbol)',
+    'Insertion (id)',
+    'Inserted element type (term)',
+    'Inserted element type (id)',
+    'Regulatory region (symbol)',
+    'Regulatory region (id)',
+    'Encoded product/allele (symbol)',
+    'Encoded product/allele (id)',
+    'Tagged with (symbol)',
+    'Tagged with (id)',
+    'Also carries (symbol)',
+    'Also carries (id)',
+    'Description (text)',
+    'Description (supporting reference)',
+    'Stocks (number)',
+    'Stock (list)'
+]
+
+# Proceed with generic setup.
+set_up_dict = set_up_db_reading(report_label)
+assembly = set_up_dict['assembly']
+annotation_release = set_up_dict['annotation_release']
+database = set_up_dict['database']
+database_release = set_up_dict['database_release']
+alliance_schema = set_up_dict['alliance_schema']
+output_dir = set_up_dict['output_dir']
+output_filename = set_up_dict['output_filename']
+log = set_up_dict['log']
+conn = set_up_dict['conn']
+the_time = set_up_dict['the_time']
+
+# Process more input parameters (-c and -v handled by set_up_db_reading() function above).
+parser = argparse.ArgumentParser(description='inputs')
+args, extra_args = parser.parse_known_args()
+log.info('Parsing args specific to this script; ignoring these: {}'.format(extra_args))
+
+
+def main():
+    """Retrieve, repackage and print out database information."""
+    log.info('Started main function.')
+    database_info = get_database_info()
+    data_to_export_as_tsv = generic_FB_tsv_dict(report_title, database)
+    data_to_export_as_tsv['data'] = process_database_info(database_info)
+    tsv_report_dump(data_to_export_as_tsv, output_filename, headers=header_list)
+    conn.close()
+    log.info('Ended main function.')
+
+
+def get_dmel_alleles():
+    """Query chado for all Dmel alleles.
+
+    Returns:
+        allele_dict (dict): A feature_id-keyed dict of alleles (represented by dicts).
+
+    """
+    global conn
+    log.info('Query chado for all Dmel alleles.')
+    fb_alleles_query = """
+        SELECT DISTINCT f.feature_id, f.name, f.uniquename
+        FROM feature f
+        JOIN organism o ON o.organism_id = f.organism_id
+        WHERE f.is_obsolete IS FALSE
+          AND f.uniquename ~ '^FBal[0-9]{7}$'
+          AND o.abbreviation = 'Dmel'
+        ORDER BY f.uniquename;
+    """
+    ret_fb_alleles = connect(fb_alleles_query, 'no_query', conn)
+    FEAT_ID = 0
+    SYMBOL = 1
+    FB_CURIE = 2
+    fb_allele_dict = {}
+    for result in ret_fb_alleles:
+        fb_allele_dict[result[FEAT_ID]] = {
+            'Allele (symbol)': result[SYMBOL],
+            'Allele (id)': result[FB_CURIE],
+            'Gene (symbol)': None,
+            'Gene (id)': None,
+            'Allele Class (term)': [],
+            'Allele Class (id)': [],
+            'Insertion (symbol)': [],
+            'Insertion (id)': [],
+            'Inserted element type (term)': [],
+            'Inserted element type (id)': [],
+            'Regulatory region (symbol)': [],
+            'Regulatory region (id)': [],
+            'Encoded product/allele (symbol)': [],
+            'Encoded product/allele (id)': [],
+            'Tagged with (symbol)': [],
+            'Tagged with (id)': [],
+            'Also carries (symbol)': [],
+            'Also carries (id)': [],
+            'Description (text)': [],
+            'Description (supporting reference)': [],
+            'Stocks (number)': 0,
+            'Stock (list)': [],
+        }
+    log.info(f'Found {len(ret_fb_alleles)} current Dmel alleles in chado.')
+    return fb_allele_dict
+
+
+def get_allele_fullnames(fb_allele_dict):
+    """Get allele full names."""
+    global conn
+    log.info('Get allele full names.')
+    fb_allele_fullnames_query = """
+        SELECT DISTINCT f.feature_id, s.name
+        FROM feature f
+        JOIN feature_synonym fs ON fs.feature_id = f.feature_id
+        JOIN synonym s ON s.synonym_id = fs.synonym_id
+        JOIN cvterm t ON t.cvterm_id = s.type_id
+        WHERE f.is_obsolete IS FALSE
+          AND f.uniquename ~ '^FBal[0-9]{7}$'
+          AND fs.is_current IS TRUE
+          AND t.name = 'fullname';
+    """
+    ret_fb_allele_fullnames = connect(fb_allele_fullnames_query, 'no_query', conn)
+    FEAT_ID = 0
+    FULLNAME = 1
+    counter = 0
+    for result in ret_fb_allele_fullnames:
+        fb_allele_dict[result[FEAT_ID]]['Name'] = result[FULLNAME]
+        counter += 1
+    log.info(f'Found {counter} current full names for experimental alleles in chado.')
+    return
+
+
+def get_database_info():
+    """Run chado db queries in sequence."""
+    global conn
+    log.info('Query database.')
+    allele_dict = get_dmel_alleles()
+    return allele_dict.values()
+
+
+def process_database_info(input_data):
+    """Process a list of data elements for TSV output.
+
+    Args:
+        input_data (list): A list of dicts representing current FB Dmel alleles.
+
+    Returns:
+        A list of dictionaries representing classical/insertion alleles.
+    """
+    log.info('Starting to process current Dmel alleles info retrieved from database.')
+    data_list = []
+    for i in input_data:
+        for k, v in i.items():
+            if type(v) is list:
+                v = '|'.join(v)
+        data_list.append(i)
+    log.info('Done processing current Dmel classical/insertion alleles into a list of dictionaries.')
+    return data_list
+
+
+if __name__ == "__main__":
+    main()
