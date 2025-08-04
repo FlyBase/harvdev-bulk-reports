520df7bc13ba49e8646dfcf067c32bcbd4a24e4e
gildossantos <gilbertodossantos74@gmail.com>
2025-08-04 16:10:20 -0400

    fix rgx?

--- src/report_hdm.py
+++ src/report_hdm.py
@@ -490,7 +490,7 @@ def get_hdm_omim_table_prop(hdm_dict):
     DB_ID = 0
     TABLE_TEXT = 1
     counter = 0
-    table_rgx = r'^\[(.+)\]\(https://omim.org/entry/[0-9]{1,8}\) +\['
+    table_rgx = r'^(\[(.*?)\])\(https://omim.org/entry/[0-9]{1,8}\) +\['
     for row in ret_hdm_omim_pheno_table_info:
         log.debug(f'BOB1: Have this prop: {row}')
         omim_table_lines = row[TABLE_TEXT].split('\n')
