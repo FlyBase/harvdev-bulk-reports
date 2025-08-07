79160d5329d5fdc7cebbb5fe2da48a227e1c9734
gildossantos <gilbertodossantos74@gmail.com>
2025-08-07 11:14:00 -0400

    group comments on single line per gene

--- src/report_gene_model_annotation_comments.py
+++ src/report_gene_model_annotation_comments.py
@@ -29,7 +29,7 @@ HEADER_LIST = [
     'FB_id',
     'Symbol',
     'Annotation_Status',
-    'Annotation_Comment',
+    'Annotation_Comments',
 ]
 
 # Proceed with generic setup.
@@ -50,38 +50,65 @@ log.info('Parsing args specific to this script; ignoring these: {}'.format(extra
 def main():
     """Retrieve, repackage and print out database information."""
     log.info('Started main function.')
-    gene_comment_list = get_gene_annotation_comments()
+    gene_dict = get_genes()
+    get_gene_annotation_comments(gene_dict)
     data_to_export_as_tsv = generic_FB_tsv_dict(REPORT_TITLE, DATABASE)
-    data_to_export_as_tsv['data'] = gene_comment_list
+    data_to_export_as_tsv['data'] = process_database_info(gene_dict)
     tsv_report_dump(data_to_export_as_tsv, OUTPUT_FILENAME, headers=HEADER_LIST)
     CONN.close()
     log.info('Ended main function.')
 
 
 # BELOW: Functions for retrieval and processing of data from chado.
-def get_gene_annotation_comments():
+def get_genes():
+    """Retrieve Dmel genes from chado."""
+    global CONN
+    log.info('Retrieve Dmel genes from chado.')
+    fb_gene_query = """
+        SELECT f.uniquename, f.name, fp.value
+        FROM feature f
+        JOIN organism o ON o.organism_id = f.organism_id
+        JOIN featureprop fp ON fp.feature_id = f.feature_id
+        JOIN cvterm c ON c.cvterm_id = fp.type_id AND c.name = 'derived_gene_model_status'
+        WHERE f.is_obsolete IS FALSE
+          AND o.abbreviation = 'Dmel'
+          AND f.uniquename ~ '^FBgn[0-9]{7}$'
+        ORDER BY f.uniquename;
+    """
+    ret_gene_info = connect(fb_gene_query, 'no_query', CONN)
+    UNAME = 0
+    NAME = 1
+    MODEL_STATUS = 2
+    gene_dict = {}
+    for row in ret_gene_info:
+        this_gene = {
+            'FB_id': row[UNAME],
+            'Symbol': row[NAME],
+            'Annotation_Status': row[MODEL_STATUS],
+            'Annotation_Comments': [],
+        }
+        gene_dict[row[UNAME]] = this_gene
+    log.info(f'Found {len(gene_dict)} genes in chado.')
+    return gene_dict
+
+
+def get_gene_annotation_comments(gene_dict):
     """Retrieve gene model annotation comments."""
     global CONN
     log.info('Retrieve gene model annotation comments.')
     fb_gene_comment_query = """
-        SELECT DISTINCT f.uniquename, f.name, fp1.value, fp2.value
+        SELECT DISTINCT f.uniquename, fp.value
         FROM feature f
         JOIN organism o ON o.organism_id = f.organism_id
-        JOIN featureprop fp1 ON fp1.feature_id = f.feature_id
-        JOIN cvterm c1 ON c1.cvterm_id = fp1.type_id AND c1.name = 'derived_gene_model_status'
-        LEFT OUTER JOIN featureprop fp2 ON fp2.feature_id = f.feature_id AND fp2.type_id IN (SELECT cvterm_id FROM cvterm WHERE name = 'comment')
+        JOIN featureprop fp ON fp.feature_id = f.feature_id
+        JOIN cvterm c ON c.cvterm_id = fp.type_id AND c.name = 'comment'
         WHERE f.is_obsolete IS FALSE
           AND f.uniquename ~ '^FBgn[0-9]{7}$'
-          AND o.abbreviation = 'Dmel'
-        ORDER BY f.uniquename, fp2.value;
+          AND o.abbreviation = 'Dmel';
     """
     ret_gene_comment_info = connect(fb_gene_comment_query, 'no_query', CONN)
     UNAME = 0
-    NAME = 1
-    STATUS = 2
-    COMMENT = 3
-    gene_comment_list = []
-    counter = 0
+    COMMENT = 1
     for row in ret_gene_comment_info:
         if row[COMMENT]:
             try:
@@ -91,16 +118,29 @@ def get_gene_annotation_comments():
                 log.error(f'For {row[UNAME]}, could not clean "{row[COMMENT]}": {e}')
         else:
             cleaned_comment = ''
-        result = {
-            'FB_id': row[UNAME],
-            'Symbol': row[NAME],
-            'Annotation_Status': row[STATUS],
-            'Annotation_Comment': cleaned_comment,
-        }
-        gene_comment_list.append(result)
+        gene_dict[row[UNAME]]['Annotation_Comments'].append(cleaned_comment)
+    log.info(f'Found {len(ret_gene_comment_info)} gene model annotation comments in chado.')
+    return
+
+
+def process_database_info(input_data):
+    """Convert the gene dict into a list of data elements."""
+    log.info('Convert the gene dict to a list of data elements.')
+    data_list = []
+    counter = 0
+    for i in input_data.values():
+        for key, value in i.items():
+            if type(value) is list:
+                if not value:
+                    i[key] = ''
+                else:
+                    value = list(set(value))
+                    value.sort()
+                    i[key] = '|'.join(value)
+        data_list.append(i)
         counter += 1
-    log.info(f'Found {counter} gene model annotation comments in chado.')
-    return gene_comment_list
+    log.info(f'Sending {counter} split system combination entries to the export file.')
+    return data_list
 
 
 if __name__ == "__main__":
